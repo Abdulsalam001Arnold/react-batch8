2b82411dac4de1202b5ef82e3df6d622437f7f15
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2026-07-01 14:11:27 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+    return(
+        <div>
+            <h1>
+                This is homepage!
+            </h1>
+        </div>
+    )
+}
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+import Header from "../components/Header"
+
+
+
+export default function Home() {
+    return(
+        <main>
+          <Header/>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias natus, deleniti recusandae deserunt iusto harum aspernatur sit quis illo quos tempore nesciunt obcaecati rerum odio perferendis a minima labore vero!
+          </p>
+        </main>
+    )
+}
